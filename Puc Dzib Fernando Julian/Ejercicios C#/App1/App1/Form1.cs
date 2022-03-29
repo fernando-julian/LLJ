adfa1bf30be6c48cd2f411c7980ae5dc1913d4e8
fernando-julian <7563@itescam.edu.mx>
2022-03-29 00:52:34 -0600

    7563

---
+++ Puc Dzib Fernando Julian/Ejercicios C#/App1/App1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App1
+{
+    public partial class Form1 : Form
+    {
+        double total;
+
+        public Form1()
+        {
+            InitializeComponent();
+            total = 0;
+        }
+        //btn aceptar
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            double temporal=0.0;
+            // Console.WriteLine("Este es ekl boton aceotar");
+            var r= double.TryParse(textBox1.Text.Trim(), out temporal);
+            if (r)
+            {
+                textBox1.Text = "";
+                total += temporal;
+            }
+            else
+            {
+                MessageBox.Show("El valor debe de ser un numerico");
+            }
+            
+            
+        }
+
+        //btn igual 
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("Este es el boton igual");
+            textBox2.AppendText(total.ToString());
+
+        }
+    }
+}
